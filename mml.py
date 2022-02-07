19fdbf17a7303dc70a79ec8a94c6fcc56e2da9c5
Swapnil Sharma <swapnil869sharma@gmail.com>
2022-02-07 18:46:14 +0530

    Initial Commit

---
+++ mml.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+import pickle
+
+df = pd.read_csv("FuelConsumption.csv")
+df.head()
+
+cdf = df[["ENGINESIZE", "CYLINDERS", "FUELCONSUMPTION_COMB", "CO2EMISSIONS"]]
+
+x = cdf.iloc[:, :3]
+y = cdf.iloc[:, -1]
+
+regressor = LinearRegression()
+regressor.fit(x, y)
+
+pickle.dump(regressor, open("model.pkl", "wb"))
